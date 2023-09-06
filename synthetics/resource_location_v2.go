cbf0bc39e40dfa31207baaa71234133690f3e9a8
jeremyh <jeremyrhicks@gmail.com>
2023-09-06 13:48:44 -0400

    country is a computed field now and not settable for private locations

--- synthetics/resource_location_v2.go
+++ synthetics/resource_location_v2.go
@@ -55,8 +55,7 @@ func resourceLocationV2() *schema.Resource {
 						},
 						"country": {
 							Type:     schema.TypeString,
-							Optional: true,
-							ForceNew: true,
+							Computed: true,
 						},
 						"default": {
 							Type:     schema.TypeBool,
