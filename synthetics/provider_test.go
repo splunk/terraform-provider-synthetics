a6c15aae578a18ca3c03bcb0b08cc4601e67d082
jeremyh <jeremyrhicks@gmail.com>
2023-11-10 09:50:52 -0500

    update acceptance tests and provider test config

--- synthetics/provider_test.go
+++ synthetics/provider_test.go
@@ -24,6 +24,21 @@ import (
 var testAccProvider *schema.Provider
 var testAccProviders map[string]*schema.Provider
 
+const (
+  // providerConfig is a shared configuration to combine with the actual
+  // test configuration so the HashiCups client is properly configured.
+  // It is also possible to use the HASHICUPS_ environment variables instead,
+  // such as updating the Makefile and running the testing through that tool.
+  providerConfig = `
+provider "synthetics" {
+	alias = "synthetics"
+	product = "observability"
+	realm = "us1"
+	# apikey = "exported as env var"
+}
+`
+)
+
 func init() {
 	testAccProvider = Provider()
 	testAccProviders = map[string]*schema.Provider{
@@ -43,7 +58,7 @@ func TestProvider_impl(t *testing.T) {
 
 func testAccPreCheck(t *testing.T) {
 	if err := os.Getenv("API_ACCESS_TOKEN"); err == "" {
-		t.Fatal("API_ACCESS_TOKEN must be set for acceptance tests")
+		t.Fatal("API_ACCESS_TOKEN must be set for acceptance tests. Set to empty string if not testing v1 rigor resources.")
 	}
 	if err := os.Getenv("OBSERVABILITY_API_TOKEN"); err == "" {
 		t.Fatal("OBSERVABILITY_API_TOKEN must be set for acceptance tests")
