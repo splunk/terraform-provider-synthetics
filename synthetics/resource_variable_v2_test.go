285e243055930962e371139fdeae490c74a6daa7
jeremyh <jeremyrhicks@gmail.com>
2023-11-13 14:48:43 -0500

    variable v2 acceptance test updats

--- synthetics/resource_variable_v2_test.go
+++ synthetics/resource_variable_v2_test.go
@@ -15,22 +15,18 @@
 package synthetics
 
 import (
-	"fmt"
-	"os"
-	"strconv"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
-	sc "github.com/splunk/syntheticsclient/v2/syntheticsclientv2"
 )
 
 const newVariableV2Config = `
 resource "synthetics_create_variable_v2" "variable_v2_foo" {
+	provider = synthetics.synthetics
   variable {
     description = "The most awesome variable. Full of snakes."
     value = "barv3v3"
-    name = "terraform-test"
+    name = "acceptance-variable-terraform-test"
     secret = false  
   }    
 }
@@ -38,10 +34,12 @@ resource "synthetics_create_variable_v2" "variable_v2_foo" {
 
 const updatedVariableV2Config = `
 resource "synthetics_create_variable_v2" "variable_v2_foo" {
+	provider = synthetics.synthetics
   variable {
-    description = "The most awesome variable. Full of snakes."
-    value = "barv3v322"
-    name = "terraform-test"
+    description = "The most awesome variable. Full of snakes and birbs."
+    value = "barv3v3"
+    name = "acceptance-variable-terraform-test"
+		// Any change to 'secret' will force re-creation of the resource
     secret = false  
   }    
 }
@@ -52,14 +50,16 @@ func TestAccCreateUpdateVariableV2(t *testing.T) {
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
 		Providers:    testAccProviders,
-		CheckDestroy: testAccVariableV2Destroy,
 		Steps: []resource.TestStep{
 			// Create It
 			{
-				Config: newVariableV2Config,
+				Config: providerConfig + newVariableV2Config,
 				Check: resource.ComposeTestCheckFunc(
-					testAccCreateUpdateVariableV2ResourceExists,
-					resource.TestCheckResourceAttr("synthetics_create_variable_v2.variable_v2_foo", "value", "barv3v3"),
+					resource.TestCheckResourceAttr("synthetics_create_variable_v2.variable_v2_foo", "variable.#", "1"),
+					resource.TestCheckResourceAttr("synthetics_create_variable_v2.variable_v2_foo", "variable.0.description", "The most awesome variable. Full of snakes."),
+					resource.TestCheckResourceAttr("synthetics_create_variable_v2.variable_v2_foo", "variable.0.value", "barv3v3"),
+					resource.TestCheckResourceAttr("synthetics_create_variable_v2.variable_v2_foo", "variable.0.name", "acceptance-variable-terraform-test"),
+					resource.TestCheckResourceAttr("synthetics_create_variable_v2.variable_v2_foo", "variable.0.secret", "false"),
 				),
 			},
 			{
@@ -70,58 +70,15 @@ func TestAccCreateUpdateVariableV2(t *testing.T) {
 			},
 			// Update It
 			{
-				Config: updatedVariableV2Config,
+				Config: providerConfig + updatedVariableV2Config,
 				Check: resource.ComposeTestCheckFunc(
-					testAccCreateUpdateVariableV2ResourceExists,
-					resource.TestCheckResourceAttr("synthetics_create_variable_v2.variable_v2_foo", "value", "barv3v322"),
+					resource.TestCheckResourceAttr("synthetics_create_variable_v2.variable_v2_foo", "variable.#", "1"),
+					resource.TestCheckResourceAttr("synthetics_create_variable_v2.variable_v2_foo", "variable.0.description", "The most awesome variable. Full of snakes and birbs."),
+					resource.TestCheckResourceAttr("synthetics_create_variable_v2.variable_v2_foo", "variable.0.value", "barv3v3"),
+					resource.TestCheckResourceAttr("synthetics_create_variable_v2.variable_v2_foo", "variable.0.name", "acceptance-variable-terraform-test"),
+					resource.TestCheckResourceAttr("synthetics_create_variable_v2.variable_v2_foo", "variable.0.secret", "false"),
 				),
 			},
 		},
 	})
 }
-
-func testAccCreateUpdateVariableV2ResourceExists(s *terraform.State) error {
-	token := os.Getenv("OBSERVABILITY_API_TOKEN")
-	realm := os.Getenv("REALM")
-	client := sc.NewClient(token, realm)
-	for _, rs := range s.RootModule().Resources {
-		switch rs.Type {
-		case "signalfx_alert_muting_rule":
-			variableId, err := strconv.Atoi(rs.Primary.ID)
-			if err != nil {
-				return fmt.Errorf("Error converting variable id: %s", err)
-			}
-			variable, _, err := client.GetVariableV2(variableId)
-			if strconv.Itoa(variable.Variable.ID) != rs.Primary.ID || err != nil {
-				return fmt.Errorf("Error finding variable v2 %s: %s", rs.Primary.ID, err)
-			}
-		default:
-			return fmt.Errorf("Unexpected resource of type: %s", rs.Type)
-		}
-	}
-	return nil
-}
-
-func testAccVariableV2Destroy(s *terraform.State) error {
-	token := os.Getenv("OBSERVABILITY_API_TOKEN")
-	realm := os.Getenv("REALM")
-	client := sc.NewClient(token, realm)
-	for _, rs := range s.RootModule().Resources {
-		switch rs.Type {
-		case "synthetics_create_variable_v2":
-			variableId, err := strconv.Atoi(rs.Primary.ID)
-			if err != nil {
-				return fmt.Errorf("Error converting variable id: %s", err)
-			}
-			variable, _, err := client.GetVariableV2(variableId)
-			if variable.Variable.ID != variableId || err != nil {
-				return fmt.Errorf("Found deleted variable %s", rs.Primary.ID)
-			}
-		default:
-			return fmt.Errorf("Unexpected resource of type: %s", rs.Type)
-		}
-	}
-
-	return nil
-}
-
