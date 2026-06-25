d23014b870bccf6890e6bddccc96ef80014264a1
Boris Valo <bvalo@splunk.com>
2026-06-25 18:20:06 +0200

    SYN-6665: address TOTP data source review feedback

--- synthetics/resource_totp_variable_v2_test.go
+++ synthetics/resource_totp_variable_v2_test.go
@@ -1,10 +1,26 @@
+// Copyright 2021 Splunk, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package synthetics
 
 import (
 	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
 func TestAccCreateTotpVariableV2(t *testing.T) {
@@ -56,6 +72,38 @@ func TestAccBrowserCheckV2WithTotpVariableReference(t *testing.T) {
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("synthetics_create_totp_variable_v2.login_mfa", "totp_variable.0.name", name),
 					resource.TestCheckResourceAttr("synthetics_create_browser_check_v2.browser_v2_totp_check", "test.0.transactions.0.steps.1.value", fmt.Sprintf("{{totp.%s}}", name)),
+				),
+			},
+		},
+	})
+}
+
+func TestAccTotpVariableV2DataSources(t *testing.T) {
+	name := "terraform_totp_datasource_mfa"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: providerConfig + testAccTotpVariableV2DataSourcesConfig(name),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrPair(
+						"data.synthetics_totp_variable_v2_check.login_mfa",
+						"id",
+						"synthetics_create_totp_variable_v2.login_mfa",
+						"id",
+					),
+					resource.TestCheckResourceAttr("data.synthetics_totp_variable_v2_check.login_mfa", "totp_variable.#", "1"),
+					resource.TestCheckResourceAttr("data.synthetics_totp_variable_v2_check.login_mfa", "totp_variable.0.name", name),
+					resource.TestCheckResourceAttr("data.synthetics_totp_variable_v2_check.login_mfa", "totp_variable.0.description", "data source MFA"),
+					resource.TestCheckResourceAttr("data.synthetics_totp_variable_v2_check.login_mfa", "totp_variable.0.digits", "6"),
+					resource.TestCheckResourceAttr("data.synthetics_totp_variable_v2_check.login_mfa", "totp_variable.0.interval", "30"),
+					resource.TestCheckResourceAttr("data.synthetics_totp_variable_v2_check.login_mfa", "totp_variable.0.hmac_digest", "sha1"),
+					resource.TestCheckNoResourceAttr("data.synthetics_totp_variable_v2_check.login_mfa", "totp_variable.0.secret"),
+					resource.TestCheckResourceAttrSet("data.synthetics_totp_variables_v2_check.all", "id"),
+					resource.TestCheckNoResourceAttr("data.synthetics_totp_variables_v2_check.all", "totp_variables.0.secret"),
+					testAccCheckTotpVariablesDataSourceContains("data.synthetics_totp_variables_v2_check.all", name),
 				),
 			},
 		},
@@ -117,4 +165,47 @@ resource "synthetics_create_browser_check_v2" "browser_v2_totp_check" {
   }
 }
 `, name)
+}
+
+func testAccTotpVariableV2DataSourcesConfig(name string) string {
+	return testAccTotpVariableV2Config(name, "data source MFA", "JBSWY3DPEHPK3PXP", 6, 30) + `
+data "synthetics_totp_variable_v2_check" "login_mfa" {
+  provider = synthetics.synthetics
+  depends_on = [synthetics_create_totp_variable_v2.login_mfa]
+
+  totp_variable {
+    id = synthetics_create_totp_variable_v2.login_mfa.totp_variable[0].id
+  }
+}
+
+data "synthetics_totp_variables_v2_check" "all" {
+  provider = synthetics.synthetics
+  depends_on = [synthetics_create_totp_variable_v2.login_mfa]
+}
+`
+}
+
+func testAccCheckTotpVariablesDataSourceContains(dataSourceName string, expectedName string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[dataSourceName]
+		if !ok {
+			return fmt.Errorf("data source %s not found", dataSourceName)
+		}
+
+		count, ok := rs.Primary.Attributes["totp_variables.#"]
+		if !ok || count == "0" {
+			return fmt.Errorf("%s has no totp_variables entries", dataSourceName)
+		}
+
+		for key, value := range rs.Primary.Attributes {
+			if strings.HasPrefix(key, "totp_variables.") && strings.HasSuffix(key, ".secret") {
+				return fmt.Errorf("%s exposed secret attribute %s", dataSourceName, key)
+			}
+			if strings.HasPrefix(key, "totp_variables.") && strings.HasSuffix(key, ".name") && value == expectedName {
+				return nil
+			}
+		}
+
+		return fmt.Errorf("%s did not include TOTP variable named %q", dataSourceName, expectedName)
+	}
 }
